425a922ca7e9f83197cd17a3a24b724e8e1b5e3a
blair49 <31095601+blair49@users.noreply.github.com>
2020-07-04 17:30:06 +0530

    Displaying message timestamp using simpledateformat

--- app/src/main/java/com/example/smackchat/adapters/MessageAdapter.kt
+++ app/src/main/java/com/example/smackchat/adapters/MessageAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.smackchat.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.smackchat.R
 import com.example.smackchat.model.Message
 import com.example.smackchat.services.UserDataService
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MessageAdapter(val context: Context, val messages: ArrayList<Message>)
                     : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
@@ -25,9 +30,22 @@ class MessageAdapter(val context: Context, val messages: ArrayList<Message>)
                         "drawable", context.packageName)
             userImage.setImageResource(resourceId)
             userImage.setBackgroundColor(UserDataService.getAvatarBgColor(message.userAvatarBgColor))
-            timeStamp.text = message.timeStamp
+            timeStamp.text = getDateString(message.timeStamp)
             userName.text = message.username
             messageBody.text = message.message
+        }
+
+        fun getDateString(isoDateString: String) : String{
+            val isoFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+            isoFormatter.timeZone = TimeZone.getTimeZone("UTC")
+            var convertedDate = Date()
+            try {
+                convertedDate = isoFormatter.parse(isoDateString)
+            } catch (e: ParseException){
+                Log.d("PARSE", "Cannot parse date")
+            }
+            val resultDateString = SimpleDateFormat("MMM d, h:mm a", Locale.getDefault())
+            return resultDateString.format(convertedDate)
         }
     }
 
